660870e291b9a4b267f7584e48231c56cfbaf755
Glen Swan <glenswan@gmail.com>
2020-10-04 14:26:49 -0400

    Added in Knight background.

--- world/systems/backgrounds/background_lookups.py
+++ world/systems/backgrounds/background_lookups.py
@@ -342,4 +342,45 @@ CHARACTER_BACKGROUNDS = {
 				  4: "I let my need to win arguments overshadow friendships and harmony.",
 				  5: "I'd risk too much to uncover a lost bit of knowledge.",
 				  6: "I like keeping secrets and won't share them with anyone."}},
+
+	"knight":
+		{"skills": {"history": [True, "int"], "persuasion": [True, "cha"]},
+		 "equiptment": [],
+		 "languages": [1, lang.LANGUAGES['standard'] + lang.LANGUAGES['exotic']],
+		 "proficiences": [1, ["dice", "dragonchess", "playing cards", "three-dragon ante"]],
+		 "traits": ["position of privilege"],
+		 "belt_pouch": 2500,
+		 "special": {},
+		 "personality trait": {1: "My eloquent flattery makes everyone I talk to feel like the most wonderful and "
+								  "important person in the world.",
+							   2: "The common folk love me for my kindness and generosity.",
+							   3: "No one could doubt by looking at my regal bearing that I am a cut "
+								  "above the unwashed masses.",
+							   4: "I take great pains to always look my best and follow the latest fashions.",
+							   5: "I don't like to get my hands dirty, and I won't be caught dead "
+								  "in unsuitable accommodations.",
+							   6: "Despite my noble birth, I do not place myself above other folk. We "
+								  "all have the same blood.",
+							   7: "My favor, once lost, is lost forever.",
+							   8: "If you do me an injury, I will crush you, ruin your name, and salt your fields."},
+		 "ideal": {1: "Respect. Respect is due to me because of my position, but all people regardless of station "
+					  "deserve to be treated with dignity.",
+				   2: "Responsibility. It is my duty to respect the authority of those above me, just as those "
+					  "below me must respect mine.",
+				   3: "Independence. I must prove that I can handle myself without the coddling of my family.",
+				   4: "Power. If I can attain more power, no one will tell me what to do.",
+				   5: "Family. Blood runs thicker than water.",
+				   6: "Noble Obligation. It is my duty to protect and care for the people beneath me."},
+		 "bond": {1: "I will face any challenge to win the approval of my family.",
+				  2: "My house's alliance with another noble family must be sustained at all costs.",
+				  3: "Nothing is more important than the other members of my family.",
+				  4: "I am in love with the heir of a family that my family despises.",
+				  5: "My loyalty to my sovereign is unwavering.",
+				  6: "The common folk must see me as a hero of the people."},
+		 "flaw": {1: "I secretly believe that everyone is beneath me.",
+				  2: "I hide a truly scandalous secret that could ruin my family forever.",
+				  3: "I too often hear veiled insults and threats in every word addressed to me, and I'm quick to anger.",
+				  4: "I have an insatiable desire for carnal pleasures.",
+				  5: "In fact, the world does revolve around me.",
+				  6: "By my words and actions, I often bring shame to my family."}},
 }
